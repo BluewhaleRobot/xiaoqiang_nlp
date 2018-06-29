690f000a1300eb28a03eb1193eb482e9401d1412
xiaoqiang <xiaoqiang@bwbot.org>
2018-06-29 10:22:46 +0800

    fix tencent sdk

--- src/engines/tencent_sdk/apiutil.py
+++ src/engines/tencent_sdk/apiutil.py
@@ -1,4 +1,4 @@
-#-*- coding: UTF-8 -*-
+# -*- coding: UTF-8 -*-
 import hashlib
 import urllib
 import urllib2
@@ -6,17 +6,19 @@ import base64
 import json
 import time
 
-url_preffix='https://api.ai.qq.com/fcgi-bin/'
+url_preffix = 'https://api.ai.qq.com/fcgi-bin/'
+
 
 def setParams(array, key, value):
     array[key] = value
+
 
 def genSignString(parser):
     uri_str = ''
     for key in sorted(parser.keys()):
         if key == 'app_key':
             continue
-        uri_str += "%s=%s&" % (key, urllib.quote(str(parser[key]), safe = ''))
+        uri_str += "%s=%s&" % (key, urllib.quote(str(parser[key]), safe=''))
     sign_str = uri_str + 'app_key=' + parser['app_key']
 
     hash_md5 = hashlib.md5(sign_str)
@@ -42,10 +44,10 @@ class AiPlat(object):
             if hasattr(e, "code"):
                 dict_error = {}
                 dict_error['ret'] = -1
-                dict_error['httpcode'] = e.code
+                # dict_error['httpcode'] = e.code
                 dict_error['msg'] = "sdk http post err"
                 return dict_error
-            if hasattr(e,"reason"):
+            if hasattr(e, "reason"):
                 dict_error['msg'] = 'sdk http post err'
                 dict_error['httpcode'] = -1
                 dict_error['ret'] = -1
@@ -56,7 +58,6 @@ class AiPlat(object):
             dict_error['httpcode'] = -1
             dict_error['msg'] = "system error"
             return dict_error
-
 
     def getOcrGeneralocr(self, image):
         self.url = url_preffix + 'ocr/ocr_generalocr'
@@ -101,7 +102,7 @@ class AiPlat(object):
         sign_str = genSignString(self.data)
         setParams(self.data, 'sign', sign_str)
         return self.invoke(self.data)
-    
+
     def getNlpChat(self, text):
         self.url = url_preffix + 'nlp/nlp_textchat'
         setParams(self.data, 'app_id', self.app_id)
@@ -114,8 +115,4 @@ class AiPlat(object):
         setParams(self.data, 'sign', sign_str)
         res = self.invoke(self.data)
         self.data = {}
-        print(res)
         return res
-        
-
-    
